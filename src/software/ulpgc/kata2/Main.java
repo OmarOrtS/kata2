704c70862a2041cd4061656350d415e6369c64af
Test <omar.ortega102@alu.ulpgc.es>
2024-10-20 16:07:24 +0100

    Title class created

--- src/software/ulpgc/kata2/Main.java
+++ src/software/ulpgc/kata2/Main.java
@@ -1,2 +1,11 @@
-package software.ulpgc.kata2;public class Main {
+package software.ulpgc.kata2;
+
+import java.io.File;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("E:/Is2/title.basics.tsv");
+        List<Title> titles = new FileTitleLoader(file, new TsvTitleDeserializer()).load();
+    }
 }
