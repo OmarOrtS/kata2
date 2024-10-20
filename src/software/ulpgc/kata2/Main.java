0a625943d31cca660b09cc0d615cbe7426525505
Test <omar.ortega102@alu.ulpgc.es>
2024-10-20 16:18:24 +0100

    kata2 finished

--- src/software/ulpgc/kata2/Main.java
+++ src/software/ulpgc/kata2/Main.java
@@ -2,11 +2,20 @@ package software.ulpgc.kata2;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("E:/Is2/title.basics.tsv");
         List<Title> titles = new FileTitleLoader(file, new TsvTitleDeserializer()).load();
+        Map<String, String> stats = new HashMap<>();
+        for (Title title : titles) {
+            stats.put(title.getTitle(), title.getGenre());
+        }
+        for (String title : stats.keySet()) {
+            System.out.println(title + "\t" + stats.get(title));
+        }
     }
 }
